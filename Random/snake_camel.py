5376d561b631b9e5a995e0b453205a359c93b1a0
thechinmayrout <thechinmayrout@gmail.com>
2021-05-15 01:14:35 -0400

    Added Casing

--- Random/snake_camel.py
+++ Random/snake_camel.py
@@ -5,11 +5,35 @@
 #PascalCase
 
 #snake_case to camelCase
-name = 'snake_case_name'
-name = ''.join(word.title() for word in name.split('_'))
+name = 'chinmayRoutIsTheBest'
+
 print(name)
 
 def camel_to_snake(s):
-    return ''.join(['_'+c.lower() if c.isupper() else c for c in s]).lstrip('_')
+    result = ""
+    c = s[0]
+    result = result + c.lower()
+
+    for i in range(1, len(s)):
+        ch = s[i]
+        if(ch.isupper()):
+            result = result + '_'
+            result = result + ch.lower()
+        else:
+            result = result + ch
+    return result
+
+
 
 print(camel_to_snake(name))
+
+def snake_to_camel(strr):
+    words = strr.split("_")
+
+    for i in range(1, len(words)):
+        words[i] = words[i].capitalize()
+
+    return "".join(words)
+
+str = "snake_case_chinmay_rout"
+print(snake_to_camel(str))
