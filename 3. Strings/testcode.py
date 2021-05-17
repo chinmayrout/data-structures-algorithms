fa1c4685f018921589c3207d713d45988d8882c5
thechinmayrout <thechinmayrout@gmail.com>
2021-05-17 14:48:24 -0400

    done

--- 3. Strings/testcode.py
+++ 3. Strings/testcode.py
@@ -1,23 +1,19 @@
 def printDuplicates(arr):
-    dict = {}
+	dict = {}
 
-    for i in arr:
-        try:
-            dict[i] += 1    #try to  add the value of (key=ith) 
-			# if the key exists in dict then increment its value(try);otherwise assign one value(except)
-        except:
-            dict[i] = 1	#now adding key-value
-	#after above for loop new dictionary is created with
-	#{10: 1, 20: 3, 30: 2, 40: 1, 50: 1, -20: 3, 60: 2}
+	for i in arr:
+		if i in dict.keys():
+			dict[i] += 1
 
-    for i in dict:
-        #if frequency is more than 1, print the element
-        if dict[i] > 1:
-            print(i,end = " ")
-			
+		else:
+			dict[i] = 1
 
-    print("\n")
+	for i in dict:
+		if dict[i] > 1:
+			print(i, end = " ")
+
+	print("\n")
 
 #Driver Code
-arry1 = [10, 20, 30, 20, 20, 30, 40, 50, -20, 60, 60, -20, -20]
-printDuplicates(arry1)
+arr = [1,1,3,2,1,2]
+printDuplicates(arr)
