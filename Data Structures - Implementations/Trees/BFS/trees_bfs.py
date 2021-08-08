98b7a3dce60f305dee94aa354555c2ab63a1eda2
thechinmayrout <thechinmayrout@gmail.com>
2021-08-08 17:10:30 -0400

    Changes

--- Data Structures - Implementations/Trees/BFS/trees_bfs.py
+++ Data Structures - Implementations/Trees/BFS/trees_bfs.py
@@ -1,55 +1,44 @@
-# Level order traversal
-# Given a binary tree, find its level order traversal.
-# Level order traversal of a tree is breadth-first traversal for the tree.
-
-# Breadth-first search (BFS) is an algorithm for traversing or searching tree or graph data structures.
-# It starts at the tree root (or some arbitrary node of a graph, sometimes referred to as a 'search key'[1]),
-# and explores all of the neighbor nodes at the present depth prior to moving on to the nodes at the next depth level.
-# https://en.wikipedia.org/wiki/Breadth-first_search
-
-# Using queue
-
-class Node:
-    # A utility function to create a new node
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
-# Iterative method to print the height of binary tree
+from collections import deque
 
 
-def printLevelOrder(root):
-    # Base Case
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+
+def traverse(root):
+    result = []
     if root is None:
-        return
+        return result
 
-    # Create an empty queue for level order traversal
-    queue = []
-
-    # Enqueue Root and initialize height
+    queue = deque()
     queue.append(root)
+    while queue:
+        levelSize = len(queue)
+        currentLevel = []
+        for _ in range(levelSize):
+            currentNode = queue.popleft()
+            # Add the node to the current level
+            currentLevel.append(currentNode.val)
+            # Insert the children of current node in the queue
+            if currentNode.left:
+                queue.append(currentNode.left)
+            if currentNode.right:
+                queue.append(currentNode.right)
 
-    while(len(queue) > 0):
-        # Prints front of the queue and remove it from queue
-        print(queue[0].data)
-        node = queue.pop(0)
-
-        # Enqueue left child
-        if node.left is not None:
-            queue.append(node.left)
-
-        # Enqueue right child
-        if node.right is not None:
-            queue.append(node.right)
+        result.append(currentLevel)
+    return result
 
 
-# Driver program to test above function
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Level Order Traversal: " + str(traverse(root)))
 
-print("Level Order Traversal of binary tree is - ")
-printLevelOrder(root)
+
+main()
---
+++ EPI/Binary Search Trees/readme.md
@@ -0,0 +1,13 @@
+# Questions in Binary Search Tree - EPI
+
+1. Test if a binary tree satisfies the BST property . . . .
+2. Find the first key greater than a given value in a BST
+3. Find the k largest elements in a BST
+4. Compute the LCA in a BST
+5. Reconstruct a BST from traversal data
+6. Find the closest entries in three sorted arrays
+7. Enumerate numbers of the form a + b''12
+8. Build a minimum height BST from a sorted affay .
+9. Test if three BST nodes are totally ordered
+10. The range lookup problem
+11. Add credits
